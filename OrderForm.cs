9fff76c73f8d8521e228049cd20beab079d22edb
Nischal Acharya <Nischal060@gmail.com>
2024-01-08 22:30:17 +0545

    orderform

---
+++ OrderForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrewHub
+{
+    public partial class OrderForm : Form
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
